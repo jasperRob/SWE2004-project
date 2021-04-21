99e8c81e18c829513f560cb464198812c196ca55
nothbagel <82931223+nothbagel@users.noreply.github.com>
2021-04-21 16:59:32 +1000

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,231 @@
+// SWE20004 Technical Software Development //
+// COVID Project //
+// Jasper Robison    #102989198 //
+// Joel Murphy       #103073746 //
+// Samuel Nothnagel  #100601319 //
+
+#include <iostream>
+#include<string>
+using namespace std;
+// #include statement list to include required C++ library functions //
+
+int main() {
+
+	int userinput, information, userchange;
+	int ID, DOB;
+	string overseas, test, status, usersymptom, address, name, symptom, location;
+	//user input variables//
+	int symptombuddy1 = 1;
+	int symptombuddy2 = 1;
+	int symptomlist[1][1];
+	int locationbuddy = 1;
+	//variables to enable the use of the database//
+	//declare all variables before proceeding to code//
+	
+	cout << "Welcome to the COVID-19 Database Management System!!" << endl;
+	cout << endl;
+	cout << "What would you like to do?" << endl;
+	cout << "Enter '1' to Enter Your Detail For COVID Test Recommendation:" << endl;
+	cout << "Enter '2' to Display High Risk Locations:" << endl;
+	cout << "Enter '3' to Display the Symptoms of COVID:" << endl;
+	cout << "Enter '4' to Display the COVID-19 Positive Patient Details:" << endl;
+	cout << "Enter '5' to Update Patient Details:" << endl;
+	cout << "Enter '6' to Quit:" << endl;
+	cin >> userinput;
+	cout << endl;
+	//prompts and takes in user input for what they wish to do//
+	
+	//covid recommendation//
+	if (userinput == 1) {
+		cout << "Do You Have Any Symptoms? (yes/no)" << endl;
+		cin >> usersymptom;
+		cout << endl;
+		cout << "Please Enter Your Details." << endl;
+		cout << "What Detail Will You Enter?" << endl;
+		cout << "Enter '1' for Patient ID:" << endl;
+		cout << "Enter '2' for Name:" << endl;
+		cout << "Enter '3' for Date of Birth:" << endl;
+		cout << "Enter '4' for Address:" << endl;
+		cout << "Enter '5' for Overseas Travel:" << endl;
+		cout << "Enter '6' for Symptoms:" << endl;
+		cout << "Enter '7' for High Risk COVID Area Location:" << endl;
+		cin >> information;
+		cout << endl;
+		//takes in user input for which information they will enter//
+
+		//following code takes in whichever information the patient has chosen to supply//
+		//and searches through the database to match with an exisitng patient//
+		if (information == 1) {
+			cout << "Enter Patient ID: ";
+			cin >> ID;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 2) {
+			cout << "Enter Name: ";
+			cin >> name;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 3) {
+			cout << "Enter Date of Birth (ddmmyy): ";
+			cin >> DOB;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 4) {
+			cout << "Enter Address (number street town postcode): ";
+			cin >> address;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 5) {
+			cout << "Overseas Travel? (yes/no): ";
+			cin >> overseas;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 6) {
+			cout << "Enter Symptoms: ";
+			cin >> symptom;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else if (information == 7) {
+			cout << "Enter location Visited: ";
+			cin >> location;
+			cout << endl;
+			cout << "Patient Information: ";
+			cout << endl;
+		}
+		else {
+			cout << "Invalid Input. Please Try Again" << endl;
+
+			main();
+		}
+		cout << endl;
+		cout << endl;
+
+		//returns to main menu//
+		main();
+	}
+	
+	//display high risk locations//
+	else if (userinput == 2) {
+		cout << "The High Risk Locations are:" << endl;
+		cout << endl;
+		cout << endl;
+
+		//return to main menu//
+		main();
+	}
+	
+	//display symptoms of covid//
+	else if (userinput == 3) {
+		cout << "The Symptoms of Covid are:" << endl;
+		cout << endl;
+		cout << endl;
+	
+		//return to main menu//
+		main();
+	}
+
+	//display covid positive patient details//
+	else if (userinput == 4) {
+		cout << "Covid Positive Patient Details are:" << endl;
+		cout << endl;
+		cout << endl;
+
+		//return to main menu//
+		main();
+	}
+
+	//update patient details//
+	else if (userinput == 5) {
+		cout << "Which Patient Details Do You Wish To Update?" << endl;
+		cout << "Please input Patient ID: ";
+		cin >> ID;
+		cout << endl;
+		//find patient data//
+		cout << "What Information Do You Wish To Change?" << endl;
+		cout << "Enter '1' for Name:" << endl;
+		cout << "Enter '2' for Address:" << endl;
+		cout << "Enter '3' for Overseas Travel:" << endl;
+		cout << "Enter '4' for Symptoms:" << endl;
+		cout << "Enter '5' for High Risk COVID Area Location:" << endl;
+		cin >> userchange;
+		cout << endl;
+
+		if (userchange == 1) {
+			cout << "Enter New Name: ";
+			cin >> name;
+			cout << endl;
+		}
+		else if (userchange == 2) {
+			cout << "Enter New Address (number street town postcode): ";
+			cin >> address;
+			cout << endl;
+		}
+		else if (userchange == 3) {
+			cout << "Change Overseas Travel? (yes/no): ";
+			cin >> overseas;
+			cout << endl;
+		}
+		else if (userchange == 4) {
+			cout << "Enter New Symptoms: ";
+			cin >> symptom;
+			cout << endl;
+		}
+		else if (userchange == 5) {
+			cout << "Enter New location Visited: ";
+			cin >> location;
+			cout << endl;
+		}
+		else {
+			cout << "Invalid Input, Goodbye!" << endl;
+		}
+
+		cout << "Updated Patient Information: ";
+		cout << endl;
+		cout << endl;
+		cout << endl;
+
+		//return to main menu//
+		main();
+	}
+
+	//quit//
+	else if (userinput == 6) {
+	cout << "Thank You For Using Our COVID-19 Database Management System!" << endl;
+	cout << "Goodbye!" << endl;
+	}
+
+	//invalid - non integer input//
+	else if (isdigit(userinput) == 0) {
+	//checks to see if it is an integer//
+	cout << "Invalid Input, Please Try Again:";
+	cout << endl;
+	cout << endl;
+
+	//return to main menu//
+	main();
+	}
+
+	//invalid integer input//
+	else {
+	cout << "Invalid Input, Please Try Again:";
+	cout << endl;
+	cout << endl;
+
+	//return to main menu//
+	main();
+}
+
+	return 0;
+}
